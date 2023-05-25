fe7fd4f0372107942305f1779b6639a077ff376d
Stanislav Sidorov <ssydorov@ukrgasbank.com>
2023-05-25 11:39:11 +0300

    final commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const bot = new TelegramApi(token, { polling: true });
 bot.setMyCommands([
   { command: "/start", description: "Приветствие" },
   { command: "/info", description: "Информация о пользователе" },
+  { command: "/ping", description: "Пнуть человечка" },
 ]);
 
 const start = () => {
@@ -14,21 +15,32 @@ const start = () => {
     const text = msg.text;
     const chatId = msg.chat.id;
 
-    if (text === "/start") {
+    if (text === "/start@ButlerHBot" || text === "/start") {
       await bot.sendAnimation(chatId, "https://i.gifer.com/7zom.gif");
       return bot.sendMessage(chatId, `Привет ${msg.from.username}`);
     }
-    if (text === "/info") {
+
+    if (text === "/info@ButlerHBot" || text === "/info") {
       return bot.sendMessage(
         chatId,
         `Тебя зовут ${msg.from.first_name || ""} ${msg.from.last_name || ""}`
       );
     }
-    await bot.sendAnimation(chatId, "https://i.gifer.com/2K8.gif");
-    return bot.sendMessage(
-      chatId,
-      "Я тебя не понимаю, сформулируй вопрос по-другому"
-    );
+
+    console.log(msg);
+    if (text === "/ping@ButlerHBot" || text === "/ping") {
+      if (msg.from.username === "SydorovStas") {
+        return bot.sendMessage(chatId, "@AnnPasss вас пнули!");
+      } else {
+        return bot.sendMessage(chatId, "@SydorovStas вас пнули!");
+      }
+    }
+
+    // await bot.sendAnimation(chatId, "https://i.gifer.com/2K8.gif");
+    // return bot.sendMessage(
+    //   chatId,
+    //   "Я тебя не понимаю, сформулируй вопрос по-другому"
+    // );
   });
 };
 
